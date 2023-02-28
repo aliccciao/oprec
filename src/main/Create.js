6374fd57214b6281f6294ef4c2e36c0bea830172
aliccciao <alicia.utomo@gmail.com>
2023-02-28 17:14:10 +0700

    implement REST api

--- src/main/Create.js
+++ src/main/Create.js
@@ -1,12 +1,52 @@
+import { useState, useEffect } from 'react';
+
 const Create = () => {
+    const [tweet, setPosts] = useState([]);
+
+    const today = new Date();
+    let time = today.getHours + ":" + today.getMinutes();
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+    let dataLength = 0;
+
+    const postTweet = () => {
+        fetch('http://localhost:8000/data')
+            .then((response) => response.json())
+            .then((data) => {
+                dataLength = data.length;
+        })
+
+        fetch('http://localhost:8000/data', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                'id': dataLength + 1,
+                'user': {
+                    'displayname': "Anonymous User",
+                    'username': "mranonymous"
+                },
+                'tweet': document.getElementById("tweet-area").value,
+                'time': time,
+                'date': date
+            })
+        }).then(() => {
+            console.log('new tweet added.')
+            window.location.replace('http://localhost:3000/');
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    }
+
     return (
         <div className="Tweet">
             <div className="h-full py-40 place-content-center justify-center items-center text-center px-12 my-auto">
             <h1 className="mb-5">What's on your mind?</h1>
                 <form>
-                    <textarea className="w-full md:w-6/12 p-2.5 text-gray-900 rounded-lg border border-gray-300" placeholder="Write your thoughts here..."></textarea>
+                    <textarea id="tweet-area" className="w-full md:w-6/12 p-2.5 text-gray-900 rounded-lg border border-gray-300" placeholder="Write your thoughts here..."></textarea>
                     <br></br>
-                    <button className="px-20 py-1 rounded-full bg-purple-500 text-white">Tweet</button>
+                    <button onClick={postTweet} className="px-20 py-1 rounded-full bg-purple-500 text-white">Tweet</button>
                 </form>
             </div>
         </div>
--- src/main/Feed.js
+++ src/main/Feed.js
@@ -1,4 +1,20 @@
+import { useState, useEffect } from 'react';
+
 const Feed = () => {
+    const [tweet, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/data')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setPosts(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
     return (
         <div className="Feed">
             <a href="/create/"className="fixed bottom-10 right-10 bg-purple-500 rounded-full p-2">
@@ -8,35 +24,24 @@ const Feed = () => {
             <div className="h-full place-content-center justify-center items-center text-center m-5 md:mx-auto mb-20">
                 <h1 className="mb-20">Welcome back, <span id="heading-1" className="underline decoration-purple-500 decoration-10">username</span>!</h1>
 
-                <div className="w-full md:w-6/12 p-6 md:p-10 md:mx-auto bg-purple-100 rounded-xl" href="/tweet/">
-                    <div className="flex mb-2">
-                        <p id="bold" className="text-xl pr-2">Alicia Here!</p>
-                        <p className="text-xl">@leeeshieu</p>
-                    </div>
-                    <p className="flex text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare, ligula varius dignissim fermentum, nulla quam semper velit, condimentum posuere elit tortor consequat mi. Aenean ullamcorper elit a ligula consequat mattis. Duis ullamcorper vel sem in gravida. Curabitur lobortis lectus nec mollis malesuada. In dui massa, facilisis eget turpis id, consectetur egestas eros. Phasellus dignissim, ligula nec pulvinar laoreet, diam dui facilisis justo, ut vehicula dolor quam et leo. Quisque pellentesque faucibus fringilla. Aliquam feugiat tempor ipsum, sed dapibus arcu rutrum varius. Vestibulum posuere, eros eu commodo feugiat, lectus ligula sollicitudin felis, eu hendrerit quam orci at sem. Mauris eu porttitor sem. Pellentesque risus libero, placerat in fermentum eu, pulvinar sit amet metus. Nunc lorem sem, fermentum id tempus at, congue ac risus. Nulla vulputate lectus a lobortis pretium. Mauris pellentesque lacinia orci nec pulvinar.</p>
-                    <hr class="h-px mt-5 bg-gray-500 border-solid border-0" />
-                    <p className="flex text-gray-700 my-3">23:59 • 29 Feb 2023</p>
-                    <hr class="h-px mb-3 bg-gray-500 border-solid border-0" />
-                    <div>
-                        <a href="/reply/" className="px-10 mx-2 py-2 rounded-full bg-purple-500 text-white hover:bg-purple-600">Reply</a>
-                        <a href="/edit/" className="px-10 mx-2 py-2 rounded-full border-2 border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-white">Edit</a>
-                    </div>
-                </div>
-                <br></br>
-                <div className="w-full md:w-6/12 p-6 md:p-10 md:mx-auto bg-purple-100 rounded-xl" href="/tweet/">
-                    <div className="flex mb-2">
-                        <p id="bold" className="text-xl pr-2">Alicia Here!</p>
-                        <p className="text-xl">@leeeshieu</p>
-                    </div>
-                    <p className="flex text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare, ligula varius dignissim fermentum, nulla quam semper velit, condimentum posuere elit tortor consequat mi. Aenean ullamcorper elit a ligula consequat mattis. Duis ullamcorper vel sem in gravida. Curabitur lobortis lectus nec mollis malesuada. In dui massa, facilisis eget turpis id, consectetur egestas eros. Phasellus dignissim, ligula nec pulvinar laoreet, diam dui facilisis justo, ut vehicula dolor quam et leo. Quisque pellentesque faucibus fringilla. Aliquam feugiat tempor ipsum, sed dapibus arcu rutrum varius. Vestibulum posuere, eros eu commodo feugiat, lectus ligula sollicitudin felis, eu hendrerit quam orci at sem. Mauris eu porttitor sem. Pellentesque risus libero, placerat in fermentum eu, pulvinar sit amet metus. Nunc lorem sem, fermentum id tempus at, congue ac risus. Nulla vulputate lectus a lobortis pretium. Mauris pellentesque lacinia orci nec pulvinar.</p>
-                    <hr class="h-px mt-5 bg-gray-500 border-solid border-0" />
-                    <p className="flex text-gray-700 my-3">23:59 • 29 Feb 2023</p>
-                    <hr class="h-px mb-3 bg-gray-500 border-solid border-0" />
-                    <div>
-                        <a href="/reply/" className="px-10 mx-2 py-2 rounded-full bg-purple-500 text-white hover:bg-purple-600">Reply</a>
-                        <a href="/edit/" className="px-10 mx-2 py-2 rounded-full border-2 border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-white">Edit</a>
-                    </div>
-                </div>
+                { tweet.map((tweet) => {
+                    return (
+                        <div className="w-full md:w-6/12 p-6 md:p-10 md:mx-auto bg-purple-100 rounded-xl my-10" href="/tweet/">
+                            <div className="flex mb-2">
+                                <p id="bold" className="text-xl pr-2">{ tweet.user.displayname }</p>
+                                <p className="text-xl">@{ tweet.user.username }</p>
+                            </div>
+                            <p className="flex text-justify">{ tweet.tweet }</p>
+                            <hr class="h-px mt-5 bg-gray-500 border-solid border-0" />
+                            <p className="flex text-gray-700 my-3">{ tweet.time } • { tweet.date }</p>
+                            <hr class="h-px mb-3 bg-gray-500 border-solid border-0" />
+                            <div>
+                                <a href="/reply/" className="px-10 mx-2 py-2 rounded-full bg-purple-500 text-white hover:bg-purple-600">Reply</a>
+                                <a href="/edit/" className="px-10 mx-2 py-2 rounded-full border-2 border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-white">Edit</a>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
